d528c7b4a98e376fd2fb093b736a7bd10a4ab0c7
anx <sebastian.glaser@digitalcareerinstitute.org>
2019-12-02 16:54:46 +0100

    Updates

---
+++ precache-manifest.ca8812d3cf8d2d59684d66a758d440d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acec03577f2c747a7ddee0a930958b87",
+    "url": "/react-redux-basic-todo/index.html"
+  },
+  {
+    "revision": "663bd10aeda3c231bf34",
+    "url": "/react-redux-basic-todo/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f5574813a8de539953a1",
+    "url": "/react-redux-basic-todo/static/css/main.64976c60.chunk.css"
+  },
+  {
+    "revision": "663bd10aeda3c231bf34",
+    "url": "/react-redux-basic-todo/static/js/2.65b9a3fa.chunk.js"
+  },
+  {
+    "revision": "f5574813a8de539953a1",
+    "url": "/react-redux-basic-todo/static/js/main.c713e0ae.chunk.js"
+  },
+  {
+    "revision": "1cd3f9870d92e77e88c3",
+    "url": "/react-redux-basic-todo/static/js/runtime-main.3d710770.js"
+  },
+  {
+    "revision": "551fcff229ccdd435f8319c93de31cb3",
+    "url": "/react-redux-basic-todo/static/media/logo.551fcff2.svg"
+  }
+]);
