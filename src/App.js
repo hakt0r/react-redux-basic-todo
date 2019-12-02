9e1a4c412e65f354e7b0e75800debe4401959c93
anx <sebastian.glaser@digitalcareerinstitute.org>
2019-12-02 11:06:42 +0100

    redux everything; prepare editor

--- src/App.js
+++ src/App.js
@@ -1,88 +1,78 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-// We need the connect function in order to create COmponents that are
-// connected to the Redux-Store, this wil magically add the dispatch prop
-// to the resulting components
-// connect()(Component) => WrappedComponentWithDispatch
 import { connect } from 'react-redux'
 
-// This is a helper function for connect, it will be the first argument
-// it's purpose is to transform the state out of redux into props
-// for the underlying component. this simple form takes whole state
-// and makes it the props object
-const mapAllProps = (state)=>{ return state }
 
-// This is and underlying Component it will be wrapped with connect, in order to
-// get the dispatch function, so we call it ToDoItem, the wrapped version will
-// be called Item
-function ToDoItem({item,index,dispatch}){
-  // we take item, index and dispatch from props, dispatch is only available
-  // because we wrap this Component with connect and then use the wrapped version
-
-  let {text, state} = item // text and state are the actual entries of a todo item
-
-  const click = ()=> {
-    // this click handler will dispatch a DEL message towards our reducer
-    // index is the actual array-index from our todo list
-    dispatch({type:"DEL",index:index})
-  }
-
-  return <div>{index} <b>{text}</b> {state} <button onClick={click}>OK</button></div>
-}
-
-// Connect our ToDoItem with the Redux-Store and call the result Item
-let Item  = connect()(ToDoItem)
-
-// This Component takes our todo-list from th Redux-Store and renders
-// and Item for each element in the array, it suppplies the index
-// so Item knows which element to remove when the DEL action is dispatched
-function ToDoItemList(props){
-  return props.todo.map( (item,index)=>{
-    return <Item item={item} index={index}/>
-  })
-}
-
-// Connect our ToDoItemList with the Redux-Store and call the result List
-// use the mapAllProps function to get the whole Redux-State into props
-let List  = connect( mapAllProps )(ToDoItemList)
-
-// Next we need a Component to add new items, hence the name :)
-// this will keep the current value of an input field in state.value
-// by updating the value using the onChange handler
-// When the USer clicks the Add button an ADD message will be dispatched
-class AddToDoItem extends React.Component {
-  state = {value:""} // define the default state
-  add = (e)=>{
-    // dispatch an ADD message using this.state.value
-    this.props.dispatch({
-      type:"ADD", value:this.state.value
-  })}
-  change = (e)=> {
-    // update state.value using the value from the event's target element
-    this.setState({value:e.target.value}) }
-  render(){
-    return (
-      <div>
-        <input onChange={this.change} value={this.state.value}/>
-        <button onClick={this.add}>Add</button>
-      </div>)
-}}
-
-// Connect our AddToDoItem with the Redux-Store and call the result Input
-let Input = connect()(AddToDoItem)
-
-function App(props) {
-  return (
+export default function App(props) { return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" width="50" />
         <Input/>
         <List/>
       </header>
-    </div>
-  );
+    </div> )}
+
+
+
+
+const mapAllProps = (state)=>{ return state }
+
+
+
+function ToDoItemList(props){
+  return props.todo.map( (item,index)=>{
+    return <Item item={item} index={index}/>
+  })
 }
 
-export default App;
+let List  = connect( mapAllProps )(ToDoItemList)
+
+
+
+function ToDoItem({item,index,dispatch}){
+  let {text, state} = item
+  const click = ()=> {
+    dispatch({type:"DEL",index:index})
+  }
+  return <div>{index} <b>{text}</b> {state} <button onClick={click}>OK</button></div>
+}
+
+let Item = connect()(ToDoItem)
+
+
+
+class EditToDoItem extends React.Component {
+
+  add = (e)=>{ this.props.dispatch({
+    type:"ADD", value:this.props.inputValue
+  })}
+
+  save = (e)=>{ this.props.dispatch({
+    type:"SAVE", value:this.props.inputValue
+  })}
+
+  change = (e)=>{ this.props.dispatch({
+    type:"UPDATE_INPUT", value:e.target.value
+  })}
+
+  render(){
+    let { editIndex, dispatch, inputValue } = this.props
+    if ( editIndex === -1 ) {
+
+      return (
+      <div>
+        <input onChange={this.change} value={inputValue}/>
+        <button onClick={this.add}>Add</button>
+      </div> )}
+    else {
+
+      return (
+      <div>
+        <input onChange={this.change} value={inputValue}/>
+        <button onClick={this.save}>Save</button>
+      </div> )
+    }
+}}
+
+let Input = connect( mapAllProps )(EditToDoItem)
--- src/index.js
+++ src/index.js
@@ -3,54 +3,54 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-// the createStore fucntion initializes a Redux-Store,
-// in order to call it we need it imported
 import { createStore } from 'redux'
-
-// The Provider Component will make out Redux-Store available to all nested
-// Conponents, lets import it, so we can use it
 import { Provider } from 'react-redux'
 
-// This is the default State of our Redux-Store,
-// we define it as a const in order to gain readability
-// we'll use it as the default value for 'state' in createStore
-let defaultState = { todo:[] }
+let defaultState = {
+  editIndex:-1,
+  inputValue:"",
+  todo:[
+    {text:'asd',status:'ok'}
+  ]
+}
 
-// The reduce function manipulates our store
-// it is the sole source of truth for the state of our application
 let reducer = (state=defaultState,action)=>{
-  // state is the current state from redux, it defaults to our defaultState
-  // action is the message coming in via dispatch, a plain object
-  //   it MUST contain type, other properties are optional
-  let {type} = action // destucture type from the action parameter
-  let newTodoList     // this will keep a copy of the todo-list
+  let {type} = action
+  let newTodoList
   switch (type){
+
+    case "UPDATE_INPUT":
+      return {...state,inputValue:action.value}
+
+    case "EDIT":
+      return {...state,editIndex:action.index}
+
+    case "SAVE":
+      newTodoList = [...state.todo]
+      newTodoList[action.index].text = action.value
+      return {...state,editIndex:-1}
+
     case "DEL":
-      newTodoList = [...state.todo]      // clone state.todo
-      newTodoList.splice(action.index,1) // cut out the item at index
-      return {todo:newTodoList};         // update state.todo with newTodoList
+      newTodoList = [...state.todo]
+      newTodoList.splice(action.index,1)
+      return {...state,todo:newTodoList};
+
     case "ADD":
-      newTodoList = state.todo.slice()   // clone state.todo, oldskool syntax
-      newTodoList.push({                 // add new item to the copy
+      newTodoList = state.todo.slice()
+      newTodoList.push({
         text: action.value,
         status: 'todo'
       })
-      return {todo:newTodoList};         // update state.todo with newTodoList
+      return {...state,todo:newTodoList};
     default: return state;
   }
 }
 
-// create a new Redux-Store using our reducer function
 let store = createStore( reducer )
 
 ReactDOM.render(
-  // Make the redux store available to all child components
   <Provider store={store}>
     <App />
   </Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
