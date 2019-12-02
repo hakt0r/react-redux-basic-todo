b91f952c9accf8fa464f4d391d3730fba23ce780
anx <sebastian.glaser@digitalcareerinstitute.org>
2019-12-02 16:47:26 +0100

    add: fancyness

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import React from 'react';
 import List   from './component/list'
 import Editor from './component/editor'
 
+import logo from './logo.svg'
+
 export default function App(props) { return (
   <div className="App">
+    <img src={logo} alt="todoReduction logo"/>
     <Editor/>
     <List/>
   </div> )}
